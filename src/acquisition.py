ec47a16c631aae25b55cbd7156411dcf86e396d7
Andrey <90399811+Andrey09477@users.noreply.github.com>
2025-06-06 16:40:37 +0300

    Dataset field accessing fixed

--- src/acquisition.py
+++ src/acquisition.py
@@ -15,8 +15,8 @@ def fill_df(spec_name, country_name, role_nums):
     for role_num in role_nums:
         role = ROLES[role_num]
         print(f'Searching for jobs by profession - {role.name}')
-        for region_id in [region_ids for region_ids in regions_df[Column.ID]]:
-            region_name = regions_df.loc[regions_df[Column.ID] == region_id][Column.NAME].values[0]
+        for region_id in [region_ids for region_ids in regions_df[Column.ID.value]]:
+            region_name = regions_df.loc[regions_df[Column.ID.value] == region_id][Column.NAME.value].values[0]
             number_of_jobs = count_jobs(spec_id, region_id, role.search_tag)
             print(f'Found {number_of_jobs} jobs by profession {role.name} and region {region_name}')
             for page_num in range(number_of_jobs // JOBS_PER_PAGE + 1):
@@ -29,11 +29,12 @@ def fill_df(spec_name, country_name, role_nums):
 # getting a dataframe of regions
 def get_regions(country_name):
     regions_df = pd.DataFrame(req.get(REQUESTS['regions']).json())
-    return pd.DataFrame(regions_df[Column.AREA][regions_df[regions_df[Column.NAME] == country_name].index.tolist()[0]])
+    region_names = regions_df[regions_df[Column.NAME.value] == country_name].index.tolist()[0]
+    return pd.DataFrame(regions_df[Column.AREA.value][region_names])
 
 # selecting jobs by specialization, region and role
 def get_jobs(spec_id, region_id, role_tag, page_num):
-    res = req.get(REQUESTS['jobs'], params = { 'search_field': Column.NAME,
+    res = req.get(REQUESTS['jobs'], params = { 'search_field': Column.NAME.value,
                                                 'specialization': spec_id,
                                                 'area': region_id,
                                                 'text': role_tag,
@@ -47,16 +48,16 @@ def get_jobs(spec_id, region_id, role_tag, page_num):
 # adding extended information to an each job (region, description, experience, key skills)
 def extend_jobs(jobs, region_name):
     for job in jobs:
-        ext_job = get_job(job[Column.ID])
-        job[Column.REGION] = region_name
-        job[Column.DESCRIPTION] = ext_job[Column.DESCRIPTION]
-        job[Column.EXPERIENCE] = ext_job[Column.EXPERIENCE]
-        job[Column.KEY_SKILLS] = ext_job[Column.KEY_SKILLS]
+        ext_job = get_job(job[Column.ID.value])
+        job[Column.REGION.value] = region_name
+        job[Column.DESCRIPTION.value] = ext_job[Column.DESCRIPTION.value]
+        job[Column.EXPERIENCE.value] = ext_job[Column.EXPERIENCE.value]
+        job[Column.KEY_SKILLS.value] = ext_job[Column.KEY_SKILLS.value]
     return jobs
     
 # counting jobs selected by specialization, region and role
 def count_jobs(spec_id, region_id, role_tag):
-    res = req.get(REQUESTS['jobs'], params = { 'search_field' : Column.NAME,
+    res = req.get(REQUESTS['jobs'], params = { 'search_field' : Column.NAME.value,
                                                 'specialization' : spec_id,
                                                 'area': region_id,
                                                 'text': role_tag,
@@ -75,4 +76,4 @@ def get_job(job_id):
 # getting a specialization id by name
 def get_spec_id(spec_name):
     spec_df = pd.DataFrame(req.get(REQUESTS['spec']).json())
-    return spec_df.loc[spec_name in spec_df[Column.NAME]][Column.ID].values[0]
+    return spec_df.loc[spec_name in spec_df[Column.NAME.value]][Column.ID.value].values[0]
